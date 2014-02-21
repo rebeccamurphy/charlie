353172c80406e4b4890171ae8924e3cb841434fb
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-02-21 18:19:10 -0500

    added charliePlugin

---
+++ CharliePlugins/src/charlie/advisor/myAdvisor.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package charlie.advisor;
+
+import charlie.card.Card;
+import charlie.card.Hand;
+import charlie.plugin.IAdvisor;
+import charlie.util.Play;
+
+/**
+ *
+ * @author Rebecca
+ */
+public class myAdvisor implements IAdvisor {
+
+    @Override
+    public Play advise(Hand myHand, Card upCard) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
