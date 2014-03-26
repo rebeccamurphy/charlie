bfa68fea48fbc27015ece9a47c8a8e985163aa76
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-03-26 16:33:49 -0400

    added some basic methods for ServerBpt

--- CharliePlugins/src/charlie/bot/server/ServerBot.java
+++ CharliePlugins/src/charlie/bot/server/ServerBot.java
@@ -6,48 +6,75 @@
 
 package charlie.bot.server;
 
+import charlie.advisor.BasicStrategy;
 import charlie.card.Card;
 import charlie.card.Hand;
 import charlie.card.Hid;
 import charlie.dealer.Dealer;
 import charlie.dealer.Seat;
+import charlie.plugin.IAdvisor;
 import charlie.plugin.IBot;
+import charlie.plugin.IPlayer;
+import charlie.util.Play;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
  * @author rebecca murphy
  */
 public class ServerBot implements IBot {
-
+    protected Hid hid;
+    protected Hand playHand;
+    protected Dealer dealer;
+    protected Seat seat;
+    protected IAdvisor advisor;
+    protected Hid dealerHid;
+    protected Card dealerUpCard;
+    protected BasicStrategy bs;
+    
+    public ServerBot () {
+    bs = new BasicStrategy();
+    }
+    
     @Override
     public Hand getHand() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return playHand;
     }
 
     @Override
     public void setDealer(Dealer dealer) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.dealer = dealer;
     }
 
     @Override
     public void sit(Seat seat) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       this.seat = seat;
+       hid = new Hid(this.seat, 0,0);
+       playHand = new Hand(hid);
     }
+    
 
     @Override
     public void startGame(List<Hid> hids, int shoeSize) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //add log of starting game? 
     }
 
     @Override
     public void endGame(int shoeSize) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //add log of in end game
     }
 
     @Override
     public void deal(Hid hid, Card card, int[] values) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if(this.dealerHid == null && hid.getSeat() ==Seat.DEALER){
+            this.dealerHid = hid;
+            this.dealerUpCard = card;
+        }
+        if (playHand.getHid() == hid && playHand.size() >2 && !(playHand.isBroke())){
+            play(hid);
+        }
+            
     }
 
     @Override
@@ -57,42 +84,78 @@ public class ServerBot implements IBot {
 
     @Override
     public void bust(Hid hid) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //add alert to tell player hand is broken 
     }
 
     @Override
     public void win(Hid hid) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //add alert for win 
     }
 
     @Override
     public void blackjack(Hid hid) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       //add alert for blackjack win
     }
 
     @Override
     public void charlie(Hid hid) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       //add alert for charlie win
     }
 
     @Override
     public void lose(Hid hid) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //add alert for lose 
     }
 
     @Override
     public void push(Hid hid) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //add alert for push 
     }
 
     @Override
     public void shuffling() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //add alert for shuffling 
     }
 
     @Override
     public void play(Hid hid) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Random random = new Random();
+        final IPlayer bot = this;
+        final Hid botHid = hid;
+        final int DELAY = random.nextInt(3001-1000) +1000;
+        final int randomPlay = random.nextInt(4);
+        final int ignoreBs = DELAY % 5;
+        final Play[] plays = {Play.DOUBLE_DOWN, Play.HIT, Play.SPLIT, Play.STAY};
+        
+        Runnable thread = new Runnable() {
+        @Override 
+        public void run() {
+            Play advice;
+            int ignoreBs = DELAY % 5;
+            try {
+                Thread.sleep(DELAY);
+            }
+            catch (InterruptedException ex) {
+              //log error  
+              }
+            if (botHid == playHand.getHid()){
+                advice = bs.getPlay(playHand, dealerUpCard);
+                
+                if (advice == Play.DOUBLE_DOWN && playHand.size() == 2)
+                    dealer.doubleDown(bot, botHid);
+                else if (advice == Play.SPLIT)
+                    dealer.hit(bot, botHid);
+                else if (advice == Play.STAY)
+                    dealer.stay(bot, botHid);
+                else
+                    dealer.hit(bot, botHid);
+                }
+            }
+                
+        };
+        new Thread(thread).start();
+    
     }
+    
     
 }
