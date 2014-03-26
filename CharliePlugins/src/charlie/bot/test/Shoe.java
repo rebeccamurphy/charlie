4d2123f13e724ea19d1d04c329e2ba7b20c0eac5
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-03-26 16:47:27 -0400

    added part 2

---
+++ CharliePlugins/src/charlie/bot/test/Shoe.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package charlie.bot.test;
+
+import java.util.Random;
+
+
+/**
+ *
+ * @author rebecca.murphy2
+ */
+public class Shoe extends charlie.card.Shoe{
+    @Override
+    public void init() {
+        ran = new Random(1);
+        load(); 
+        shuffle();
+        
+    }
+}
